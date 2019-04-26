ba7e28cebea9f86c763d71eba33b3cc77ba095bd
oscar <oscaroceguera@gmail.com>
2019-04-26 02:37:46 -0600

    Test if there are words guessed

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {shallow} from 'enzyme'
 import {findByTestAttr, checkProps} from '../test/testUtils'
 import GuessedWords from './GuessedWords'
+import { wrap } from 'module';
 
 const defaulProps = {
   guessedWords: [ {guessedWord: 'train', letterMatchCount: 3} ]
@@ -34,6 +35,28 @@ describe('if there are no words guessed', () => {
 })
 
 describe('if there are words guessed', () => {
+  let wrapper
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 }
+  ]
+  beforeEach(() => {
+    wrapper = setup({ guessedWords})
+  })
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words')
+    expect(component.length).toBe(1)
+  })
 
+  test('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words')
+    expect(guessedWordsNode.length).toBe(1)
+  })
+
+  test('correct number of guessed words', () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word')
+    expect(guessedWordsNodes.length).toBe(guessedWords.length)
+  })
 })
 
