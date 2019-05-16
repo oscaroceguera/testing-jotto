578a10a2787906ed1d4e958dd47b807cd3536ea7
oscar <oscaroceguera@gmail.com>
2019-05-16 01:41:51 -0600

    Thunk integration finish

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import {combineReducers} from 'redux'
 import success from './sucessReducer'
+import guessedWords from './guessedWordsReducer'
+import secretWord from './secretWordReducer'
 
 export default combineReducers({
-  success
+  success,
+  guessedWords,
+  secretWord
 })
